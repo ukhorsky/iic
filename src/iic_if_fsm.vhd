03e8b682f6f45703bc78070be7da9c27520f3a84
Tim <tim@ya.ru>
2020-05-17 01:53:44 +0300

    byte processing fsm started

--- src/iic_if_fsm.vhd
+++ src/iic_if_fsm.vhd
@@ -7,11 +7,13 @@ entity iic_if_fsm is
     port(
         clk : in std_logic;
         cs : in std_logic;
-        d_in : in std_logic_vector(7 downto 0);
-        sda_in : in std_logic;
-        scl_pd : in std_logic;
-        d_out : out std_logic_vector(7 downto 0);        
+        w : in std_logic;
+        byte : in std_logic;
+        sda : in std_logic;
+        scl_int : in std_logic;
+        scl_ce : out std_logic;
         sda_pd : out std_logic;
+        nb : out std_logic;
         rst : in std_logic
     );
 end iic_if_fsm;
@@ -21,24 +23,41 @@ architecture rtl of iic_if_fsm is
     type state is (S0, S1, S2, S3, S4);
     
     signal c_state, n_state : state;
-    signal d_buf : std_logic_vector(8 downto 0);
-    signal byte_type : std_logic_vector(1 downto 0); -- 
 
 begin
 
     process (clk)
     begin
-        if (rising_edge(clk)) then
+        if (rst = '1') then
+            c_state <= S0;
+        elsif (rising_edge(clk)) then
             c_state <= n_state;
         end if;
     end process;
 
-    process (c_state)
+    process (c_state, cs, byte, sda, scl_int)
     begin
         case c_state is
             when S0 =>
+                if (cs = '1') then
+                    n_state <= S1;
+                else
+                    n_state <= S0;
+                end if;
             when S1 =>
+                if (scl_int = '0') then                
+                    n_state <= S2;
+                else
+                    n_stae <= S1;
+                end if;
             when S2 =>
+            when S3 =>
+            when S4 =>
+                if (scl_int = '1') then                
+                    n_state <= S0;
+                else
+                    n_stae <= S4;
+                end if;
             when others =>
                 null;
         end case;
@@ -46,8 +65,32 @@ begin
     
     process (clk)
     begin
-        if (rising_edge(clk)) then
+        if (rst = '1') then
+            scl_ce <= '0';
+            sda_pd <= '0';
+            nxt <= '0';
+        elsif (rising_edge(clk)) then
             case c_state is
+                when S0 =>
+                    scl_ce <= '0';
+                    sda_pd <= '0';
+                    nxt <= '0';
+                when S1 =>
+                    scl_ce <= '1';
+                    sda_pd <= '1';
+                    nxt <= '0';
+                when S2 =>
+                    scl_ce <= '1';
+                    sda_pd <= '0';
+                    nxt <= '0';
+                when S3 =>
+                    scl_ce <= '1';
+                    sda_pd <= '0';
+                    nxt <= '0';
+                when S4 =>
+                    scl_ce <= '0';
+                    sda_pd <= '0';
+                    nxt <= '0';
                 when others =>
                     null;
             end case;
